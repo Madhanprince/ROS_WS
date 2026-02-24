b4bd1dc6ea5937fcd763b86aeafc8faaa28df2c0
madhan <madjeeve123@gmail.com>
2026-02-24 17:22:11 +0530

    new

---
+++ src/build/action_tutorials_cpp/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/maddy/qt_ros_workspace/src/install/action_tutorials_interfaces/share/action_tutorials_interfaces/package.sh"
